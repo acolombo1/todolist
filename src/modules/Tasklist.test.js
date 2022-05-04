98a12358c8de1e70d8f3ae595267c270c3fce1ee
jr-cast <castanos@pm.me>
2022-05-04 15:45:51 -0500

    Add jsdom dependency

--- src/modules/Tasklist.test.js
+++ src/modules/Tasklist.test.js
@@ -5,13 +5,17 @@ const tasklist1 = new Tasklist();
 test('Add one new item to the list', () => {
   document.body.innerHTML = '<div class="listcontainer">'
     + '<ul class="mainlist">'
-      + '<li>Today\'s To Do</li>'
-      + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
-      + '<li class="divfinal">Clear all completed</li>'
+    + '<li>Today\'s To Do</li>'
+    + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
+    + '<li><div class="divfinal">Clear all completed</div></li>'
     + '</ul>'
-  + '</div>';
+    + '</div>';
   const divfinal = document.querySelector('.divfinal');
   tasklist1.createli('description', false, 1, divfinal);
-  const list = document.querySelectorAll('#list li');
-  expect(list).toHaveLength(1);
+  const list = document.querySelector('.mainlist');
+  expect(list).toHaveLength(4);
 });
+
+
+
+
