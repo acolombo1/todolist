da4249a9676a92aa16c2f718ebd6704829d60cd2
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-05-06 21:27:20 -0300

    move text to a const

--- src/modules/Tasklist2.test.js
+++ src/modules/Tasklist2.test.js
@@ -1,28 +1,25 @@
 import Tasklist from './Tasklist.js';
 
+const htmlinner = '<div class="listcontainer">'
++ '<ul class="mainlist">'
++ '<li>Today\'s To Do</li>'
++ '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
++ '<li id="li0"><input type="checkbox"><textarea class="description tachado" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
++ '<li id="li1"><input type="checkbox"><textarea class="description" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
++ '<li><div class="divfinal">Clear all completed</div></li>'
++ '</ul>'
++ '</div>';
 const tasklist2 = new Tasklist();
 
 describe('test input text', () => {
   test('Updates input text', () => {
-    tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }];
-    document.body.innerHTML = '<div class="listcontainer">'
-      + '<ul class="mainlist">'
-      + '<li>Today\'s To Do</li>'
-      + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
-      + '<li id="li0"><input type="checkbox"><textarea class="description tachado" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li id="li1"><input type="checkbox"><textarea class="description" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li><div class="divfinal">Clear all completed</div></li>'
-      + '</ul>'
-      + '</div>';
+    document.body.innerHTML = htmlinner;
     const divfinal = document.querySelector('.divfinal');
     tasklist2.createli('sth more', false, 2, divfinal);
     tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }, { description: 'sth more', completed: false, index: 3 }];
     const list = document.querySelector('.mainlist');
     expect(list.children).toHaveLength(6);
     const input = document.querySelectorAll('textarea')[2];
-    input.focus();
-    input.dispatchEvent(new KeyboardEvent('keypress', { key: 'abcd' }));
-    input.dispatchEvent(new KeyboardEvent('keypress', { key: 'Enter' }));
     input.value = 'abcd';
     input.dispatchEvent(new InputEvent('change'));
     expect(input.value).toBe('abcd');
@@ -32,15 +29,7 @@ describe('test input text', () => {
 
 describe('test change checked', () => {
   test('test change F to T', () => {
-    document.body.innerHTML = '<div class="listcontainer">'
-      + '<ul class="mainlist">'
-      + '<li>Today\'s To Do</li>'
-      + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
-      + '<li id="li0"><input type="checkbox"><textarea class="description tachado" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li id="li1"><input type="checkbox"><textarea class="description" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li><div class="divfinal">Clear all completed</div></li>'
-      + '</ul>'
-      + '</div>';
+    document.body.innerHTML = htmlinner;
     const divfinal = document.querySelector('.divfinal');
     tasklist2.createli('sth more', false, 2, divfinal);
     tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }, { description: 'sth more', completed: false, index: 3 }];
@@ -54,15 +43,7 @@ describe('test change checked', () => {
   });
 
   test('test change T to F', () => {
-    document.body.innerHTML = '<div class="listcontainer">'
-      + '<ul class="mainlist">'
-      + '<li>Today\'s To Do</li>'
-      + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
-      + '<li id="li0"><input type="checkbox"><textarea class="description tachado" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li id="li1"><input type="checkbox"><textarea class="description" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li><div class="divfinal">Clear all completed</div></li>'
-      + '</ul>'
-      + '</div>';
+    document.body.innerHTML = htmlinner;
     const divfinal = document.querySelector('.divfinal');
     tasklist2.createli('sth more', true, 2, divfinal);
     tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }, { description: 'sth more', completed: true, index: 3 }];
@@ -78,16 +59,7 @@ describe('test change checked', () => {
 
 describe('test clearcompleted', () => {
   test('test clearcompleted 1', () => {
-    tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }];
-    document.body.innerHTML = '<div class="listcontainer">'
-      + '<ul class="mainlist">'
-      + '<li>Today\'s To Do</li>'
-      + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
-      + '<li id="li0"><input type="checkbox"><textarea class="description tachado" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li id="li1"><input type="checkbox"><textarea class="description" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li><div class="divfinal">Clear all completed</div></li>'
-      + '</ul>'
-      + '</div>';
+    document.body.innerHTML = htmlinner;
     const divfinal = document.querySelector('.divfinal');
     tasklist2.createli('sth more', false, 2, divfinal);
     tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }, { description: 'sth more', completed: false, index: 3 }];
@@ -100,15 +72,7 @@ describe('test clearcompleted', () => {
   });
 
   test('test clearcompleted more than 1', () => {
-    document.body.innerHTML = '<div class="listcontainer">'
-      + '<ul class="mainlist">'
-      + '<li>Today\'s To Do</li>'
-      + '<li class="addli"><div class="addmsg">Add to your list...</div></li>'
-      + '<li id="li0"><input type="checkbox"><textarea class="description tachado" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li id="li1"><input type="checkbox"><textarea class="description" rows="1" style="height: 15px;">Go to the Grocer\'s</textarea></li>'
-      + '<li><div class="divfinal">Clear all completed</div></li>'
-      + '</ul>'
-      + '</div>';
+    document.body.innerHTML = htmlinner;
     const divfinal = document.querySelector('.divfinal');
     tasklist2.createli('sth more', true, 2, divfinal);
     tasklist2.tasks = [{ description: 'sth', completed: true, index: 1 }, { description: 'any', completed: false, index: 2 }, { description: 'sth more', completed: true, index: 3 }];
